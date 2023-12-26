a8c611ef0ee5888c3ba7341c5a4fabe077cf20ad
VinayakGaikwad <vinaayakgaikwad@gmail.com>
2023-12-26 11:53:28 +0530

    Added all NextJs files

---
+++ src/app/login/page.jsx
@@ -0,0 +1,7 @@
+export default function loginPage() {
+    return (
+        <div className="flex ">
+            <h1 className="text-center text-black text-2xl ">Login Here</h1>
+        </div>
+    )
+}
---
+++ src/app/signup/page.jsx
@@ -0,0 +1,7 @@
+export default function signupPage() {
+    return (
+        <div className="flex ">
+            <h1 className="text-center text-black text-2xl ">Sign Up Here people</h1>
+        </div>
+    )
+}
